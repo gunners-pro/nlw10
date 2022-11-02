d49785474ee44ca4b564aecb934f616d4d268dcf
Fabricyo Barreto <bass.barreto@gmail.com>
2022-11-02 11:44:59 -0300

    chore: mobile: add custom themes and configured fonts

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Center, Text } from 'native-base';
+
+export function SignIn() {
+  return (
+    <Center bg="gray.900" flex={1}>
+      <Text color="white">SignIn</Text>
+    </Center>
+  );
+}
