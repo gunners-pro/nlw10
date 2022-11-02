212a75fcef1b610974906a1c13f5e22307a615e9
Fabricyo Barreto <bass.barreto@gmail.com>
2022-11-02 16:13:39 -0300

    style: mobile: add button component and style to signIn screen

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Center } from 'native-base';
+import { Fontisto } from '@expo/vector-icons';
+import { Center, Icon, Text } from 'native-base';
 
 import Logo from '../assets/logo.svg';
+import { Button } from '../components/Button';
 
 export function SignIn() {
   return (
-    <Center bg="gray.900" flex={1}>
+    <Center bg="gray.900" flex={1} p={7}>
       <Logo width={212} height={40} />
+      <Button
+        title="Entrar com google"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        type="SECONDARY"
+        mt={12}
+      />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além{'\n'}do seu e-mail para criação
+        de sua conta.
+      </Text>
     </Center>
   );
 }
