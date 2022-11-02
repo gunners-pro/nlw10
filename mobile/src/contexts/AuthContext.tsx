5c97c21171e32aa978da27aae36bd2337cac8d6a
Fabricyo Barreto <bass.barreto@gmail.com>
2022-11-02 19:42:55 -0300

    chore: mobile: add config to get token from Google authentication

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,16 @@
-import { createContext, useMemo } from 'react';
+import {
+  createContext,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
+import { GOOGLE_CLIENTE_ID } from 'react-native-dotenv';
+import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface User {
   name: string;
@@ -7,6 +19,7 @@ interface User {
 
 interface AuthContextDataProps {
   user: User;
+  isUserLoading: boolean;
   signIn: () => Promise<void>;
 }
 
@@ -15,17 +28,44 @@ export const AuthContext = createContext<AuthContextDataProps>(
 );
 
 export function AuthProvider({ children }) {
-  async function signIn() {
-    // todo
+  const [isUserLoading, setIsUserLoading] = useState(false);
+  const [user, setUser] = useState<User>({} as User);
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: GOOGLE_CLIENTE_ID,
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email'],
+  });
+
+  async function signInWithGoogle(accessToken: string) {
+    console.log('TOKEN DE AUTENTICAÇÃO ===> ', accessToken);
   }
+
+  const signIn = useCallback(async () => {
+    try {
+      setIsUserLoading(true);
+      await promptAsync();
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
+  }, [promptAsync]);
 
   const contextData = useMemo<AuthContextDataProps>(
     () => ({
       signIn,
-      user: { name: 'Fabricyo', avatarUrl: '' },
+      user,
+      isUserLoading,
     }),
-    []
+    [isUserLoading, signIn, user]
   );
+
+  useEffect(() => {
+    if (response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
 
   return (
     <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -4,8 +4,11 @@ import { Center, Icon, Text } from 'native-base';
 
 import Logo from '../assets/logo.svg';
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 export function SignIn() {
+  const { signIn } = useAuth();
+
   return (
     <Center bg="gray.900" flex={1} p={7}>
       <Logo width={212} height={40} />
@@ -14,6 +17,7 @@ export function SignIn() {
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
         type="SECONDARY"
         mt={12}
+        onPress={signIn}
       />
 
       <Text color="white" textAlign="center" mt={4}>
