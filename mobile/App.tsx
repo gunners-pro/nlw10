41583a32c6bb220a8ffe3bcaa5dbfab2dbb4c47b
Fabricyo Barreto <bass.barreto@gmail.com>
2022-11-02 12:11:11 -0300

    chore: mobile: add eslint config and applied lint

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 import { THEME } from './src/styles/theme';
 import { Loading } from './src/components/Loading';
 import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
 
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar style="light" />
-      {
-        fontsLoaded ? <SignIn /> : <Loading />
-      }
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
