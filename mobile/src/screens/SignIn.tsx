7a696ecfd3f981d53d61003756ebf97bda1d0a6b
Fabricyo Barreto <bass.barreto@gmail.com>
2022-11-02 15:45:06 -0300

    chore: mobile: add config svg and put the logo on signIn

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Center, Text } from 'native-base';
+import { Center } from 'native-base';
+
+import Logo from '../assets/logo.svg';
 
 export function SignIn() {
   return (
     <Center bg="gray.900" flex={1}>
-      <Text color="white">SignIn</Text>
+      <Logo width={212} height={40} />
     </Center>
   );
 }
