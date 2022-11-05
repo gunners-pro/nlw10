2fd2ca7b862cdb6063af3de57af8a122dcbe6f6f
Fabricyo Barreto <bass.barreto@gmail.com>
2022-11-04 22:08:52 -0300

    feat: backend: add route auth and geration of tokens

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
 import Fastify from "fastify";
 
+import { authRoutes } from "./routes/auth";
 import { guessRoutes } from "./routes/guess";
 import { poolRoutes } from "./routes/pool";
 import { userRoutes } from "./routes/user";
@@ -14,9 +16,14 @@ async function bootstrap() {
     origin: true,
   });
 
+  await fastify.register(jwt, {
+    secret: "nlwcopa",
+  });
+
   await fastify.register(poolRoutes);
   await fastify.register(userRoutes);
   await fastify.register(guessRoutes);
+  await fastify.register(authRoutes);
 
   await fastify.listen({ port: 3333, host: "0.0.0.0" });
 }
