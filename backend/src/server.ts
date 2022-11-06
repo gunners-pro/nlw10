0c6aa2691296bf182b98f527163437c70bd36e0a
Fabricyo Barreto <bass.barreto@gmail.com>
2022-11-06 12:06:47 -0300

    feat: backend: add route that list games in a pool.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import jwt from "@fastify/jwt";
 import Fastify from "fastify";
 
 import { authRoutes } from "./routes/auth";
+import { gameRoutes } from "./routes/game";
 import { guessRoutes } from "./routes/guess";
 import { poolRoutes } from "./routes/pool";
 import { userRoutes } from "./routes/user";
@@ -24,6 +25,7 @@ async function bootstrap() {
   await fastify.register(userRoutes);
   await fastify.register(guessRoutes);
   await fastify.register(authRoutes);
+  await fastify.register(gameRoutes);
 
   await fastify.listen({ port: 3333, host: "0.0.0.0" });
 }
