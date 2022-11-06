26575f44c78598537f7851db8ddfda555382ab99
Fabricyo Barreto <bass.barreto@gmail.com>
2022-11-06 17:07:58 -0300

    feat: mobile: add the function that create new pool

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
-import { Text, VStack } from 'native-base';
+import React, { useState } from 'react';
+import { Text, useToast, VStack } from 'native-base';
 import { Header } from '../components/Header';
 import Logo from '../assets/logo.svg';
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
+import { api } from '../services/api';
 
 export function New() {
+  const [title, setTitle] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+
+  async function handlePoolCreate() {
+    if (!title.trim()) {
+      return toast.show({
+        title: 'Informe um nome para o seu bolão',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
+
+    try {
+      setIsLoading(true);
+      await api.post('/pools', { title });
+      toast.show({
+        title: 'Bolão criado com sucesso',
+        placement: 'top',
+        bgColor: 'green.500',
+      });
+
+      setTitle('');
+    } catch (error) {
+      toast.show({
+        title: 'Error',
+        description: error.message,
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <VStack flex={1} bg="gray.900">
       <Header title="Criar novo bolão" />
@@ -22,8 +58,17 @@ export function New() {
           Crie seu próprio bolão da copa{'\n'}e compartilhe entre amigos!
         </Text>
 
-        <Input mb={2} placeholder="Qual nome do seu bolão ?" />
-        <Button title="Criar meu bolão" />
+        <Input
+          mb={2}
+          placeholder="Qual nome do seu bolão ?"
+          value={title}
+          onChangeText={setTitle}
+        />
+        <Button
+          title="Criar meu bolão"
+          onPress={handlePoolCreate}
+          isLoading={isLoading}
+        />
 
         <Text color="gray.200" textAlign="center" px={10} mt={4}>
           Após criar seu bolão, você receberá{'\n'}um código único que poderá
