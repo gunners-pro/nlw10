91c8f86d7a57ab4ed6ba915635855143210cd492
Fabricyo Barreto <bass.barreto@gmail.com>
2022-11-06 23:12:50 -0300

    chore: mobile: add the right config enviroment variable.

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,10 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    "plugins": [
-      ["module:react-native-dotenv", {
-        "moduleName": "react-native-dotenv"
-      }]
-    ]
+    plugins: ['inline-dotenv']
   };
 };
